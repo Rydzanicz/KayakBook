d379f876788866f6662a69d8388bb3bcbc994fc1
Rydzanicz <asddsfsdfsf@gmail.com>
2025-03-18 17:01:24 +0100

    Added image gallery component with navigation, improved styles, and updated routes and assets

--- kayakBook/src/app/body/body.component.ts
+++ kayakBook/src/app/body/body.component.ts
@@ -1,6 +1,6 @@
-import {Component} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {RouterLink} from '@angular/router';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 interface Trace {
   id: string;
@@ -10,16 +10,28 @@ interface Trace {
 @Component({
   selector: 'app-body',
   templateUrl: './body.component.html',
-  imports: [
-    CommonModule,
-    RouterLink
-  ],
-  styleUrls: ['./body.component.css']
+  imports: [CommonModule, RouterLink],
+  styleUrls: ['./body.component.css'],
 })
 export class BodyComponent {
   traces: Trace[] = [
-    {id: 'Prawiedniki_Zemborzycki', description: 'Prawiedniki - Z. Zemborzycki'},
-    {id: 'Osmolice_Prawiedniki', description: 'Osmolice - Prawiedniki'},
-    {id: 'Osmolice_Zemborzycki', description: 'Osmolice - Z. Zemborzycki'}
+    { id: 'Prawiedniki_Zemborzycki', description: 'Prawiedniki - Z. Zemborzycki' },
+    { id: 'Osmolice_Prawiedniki', description: 'Osmolice - Prawiedniki' },
+    { id: 'Osmolice_Zemborzycki', description: 'Osmolice - Z. Zemborzycki' },
   ];
+  galleryImages = [
+    'assets/gallery/image1.png',
+    'assets/gallery/image2.png',
+    'assets/gallery/image3.png',
+  ];
+  currentIndex = 0;
+
+  prevImage() {
+    this.currentIndex =
+      (this.currentIndex - 1 + this.galleryImages.length) % this.galleryImages.length;
+  }
+
+  nextImage() {
+    this.currentIndex = (this.currentIndex + 1) % this.galleryImages.length;
+  }
 }
